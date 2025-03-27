4d679f956a401a59431659edcff0877de8849b47
Vladimir <wlrybas@gmail.com>
2025-03-27 19:40:23 +0300

    fix deploy again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://granondo.github.io',
   base: '/browser-picker-site',
-})
+  build: {
+    assets: '_assets',
+    inlineStylesheets: 'never'
+  },
+  vite: {
+    build: {
+      cssCodeSplit: false
+    }
+  }
+});
+
 
