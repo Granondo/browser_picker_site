05a9ab51f19ae106962bf9c201354cd91a8df077
Vladimir <wlrybas@gmail.com>
2025-03-27 19:49:32 +0300

    fix deploy styling again

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,9 @@ export default defineConfig({
   site: 'https://granondo.github.io',
   base: '/browser-picker-site',
   build: {
-    assets: '_assets',
-    inlineStylesheets: 'never'
-  },
-  vite: {
-    build: {
-      cssCodeSplit: false
-    }
+    assets: 'assets'
   }
 });
+
 
 
