90dbcfc0f4e749d5677c776b657813e2ef672c0d
Vladimir <wlrybas@gmail.com>
2025-03-28 18:22:00 +0300

    another one try to fix deploy css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://granondo.github.io',
+  base: '/browser_picker_site', // Note the underscore, not hyphen
 })
+
 
 
 
