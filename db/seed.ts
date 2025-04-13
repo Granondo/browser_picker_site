150a8ff9695f149c765196142f7e9cd32a85fade
Vladimir <wlrybas@gmail.com>
2025-04-13 17:32:30 +0300

    update icon and comment db

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,10 @@
-import { db } from 'astro:db';
+import { db, moonClicks } from 'astro:db';
 
-// https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(moonClicks).values({
+    totalClicks: 0,
+    lastUpdated: new Date()
+  });
 }
+
+
